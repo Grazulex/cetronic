4c265fd98c94acaa73b206c2090833f7bee0bb60
jean-marc strauven <jms@grazulex.be>
2024-07-05 18:08:32 +0200

    change taille pdf

--- resources/views/pdf/catalog.blade.php
+++ resources/views/pdf/catalog.blade.php
@@ -2,48 +2,45 @@
 <html>
 <head>
     <link rel="stylesheet" href="{{ base_path('public/Css/pdf.css') }}">
-    <link rel="stylesheet" href="{{ base_path('public/Css/simple-grid.css') }}">
 </head>
 <body>
-<div class="container">
+<div class="section-container">
     @php
         $rowCount = 0;
         $nextPage = 0;
     @endphp
-    <div class="row">
+    <section class="top-section">
         @foreach($products as $product)
-            <div class="col-2 item">
-                <div>
-                    <div class="image"><img src="{{$product->first_media_path}}"></div>
-                    <div class="product-data">
-                        <div class="sku"><b>{{strtoupper($product->reference)}}</b></div>
-                        <div><b>{{$product->price}}€</b></div>
-                        @foreach($product->metas as $meta)
-                            <div><b>{{ucfirst($meta->meta->name)}}: </b>{{$meta->value}}</div>
-                        @endforeach
-                    </div>
+            <div class="column">
+                <div class="image"><img src="{{$product->first_media_path}}"></div>
+                <div class="product-data">
+                    <div class="sku"><b>{{strtoupper($product->reference)}}</b></div>
+                    <div><b>{{$product->price}}€</b></div>
+                    @foreach($product->metas as $meta)
+                        <div><b>{{ucfirst($meta->meta->name)}}: </b>{{$meta->value}}</div>
+                    @endforeach
+
+                    @php
+                        $rowCount++;
+                    @endphp
+                    @if ($rowCount == 6)
+                        @php
+                            $nextPage++;
+                            $rowCount = 0;
+                        @endphp
                 </div>
             </div>
-            @php
-                $rowCount++;
-            @endphp
-            @if ($rowCount == 6)
+            @if ($nextPage == 2)
+                <div class="page-break"></div>
                 @php
-                    $nextPage++;
-                    $rowCount = 0;
+                    $nextPage = 0;
                 @endphp
-    </div>
-    @if ($nextPage == 2)
-        <div class="page-break"></div>
-        <div class="page-header"></div>
-        @php
-            $nextPage = 0;
-        @endphp
-    @endif
-    <div class="row">
-        @endif
-        @endforeach
-    </div>
+            @endif
+            <section class="top-section">
+                @endif
+                @endforeach
+            </section>
+    </section>
 </div>
 </body>
 </html>
