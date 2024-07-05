aed97ac48049188717a172167295a5d6917b9918
jean-marc strauven <jms@grazulex.be>
2024-07-05 18:32:22 +0200

    change pdf

--- resources/views/pdf/catalog.blade.php
+++ resources/views/pdf/catalog.blade.php
@@ -1,7 +1,80 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="{{ base_path('public/Css/pdf.css') }}">
+    <style>
+        /* styles.css */
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body {
+            font-family: Arial, sans-serif;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 0;
+            text-align: center;
+        }
+
+        header nav ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+            text-align: center;
+        }
+
+        header nav ul li {
+            display: inline;
+            margin-right: 20px;
+        }
+
+        header nav ul li a {
+            color: #fff;
+            text-decoration: none;
+            font-weight: bold;
+        }
+
+        .section-container {
+            display: flex;
+            flex-direction: column;
+            height: 100vh;
+        }
+
+        .page {
+            height: 100vh;
+            page-break-after: always;
+        }
+
+        .row {
+            display: flex;
+            flex: 1;
+        }
+
+        .column {
+            flex: 1;
+            border: 1px solid #ccc;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 10px;
+            background-color: #fff;
+        }
+
+        footer {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px 0;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
 <div class="section-container">
